dbeb76a50e8448d7d376bb28783223406a3c9753
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2020-09-01 01:59:17 +0200

    dsa
    z

--- bowser.js
+++ bowser.js
@@ -1,4 +1,7 @@
-var p = document.getElementById("info")	
+var p = document.getElementById("name-version")	
+var engine = document.getElementById("engine")	
+var os = document.getElementById("os")	
+var platform = document.getElementById("info")	
 
 window.addEventListener("load", function() {
     // PARSE USER AGENT
@@ -16,8 +19,13 @@ window.addEventListener("load", function() {
    
     // PLATFORM
     console.log(result.platform.type);
+    console.log(result.userAgent)
 
-    p.innerHTML = (result.browser.name +"\n"+ result.browser.version)
+    p.innerHTML = ("Browser Name : "+result.browser.name +" -- \n VERSION : "+ result.browser.version);
+    engine.innerHTML = ("engine : "+result.browser.engine);
+    os.innerHTML = ("os Name : "+result.os.name +" -- \n os version : "+ result.os.version+"VersionName"+ result.os.versionName)
+    platform.innerHTML = ("os Name : "+result.platform.type);
+
   });
 
 
